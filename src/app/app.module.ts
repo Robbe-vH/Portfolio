a4b585328096c3b7ae53daa0223b23f1fab409ea
Robbe van Hoppe <robbe.vanhoppe@student.pxl.be>
2022-10-25 00:03:07 +0200

    basic navbar toegevoegd en resume pagina erin geplakt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Navbar } from './navbar/navbar.component';
+import { ResumePagina } from './resume/resume.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Navbar,
+    ResumePagina
   ],
   imports: [
     BrowserModule
---
+++ src/app/resume/resume.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'portfolio-ResumePagina',
+    templateUrl: './resume.component.html',
+    styleUrls: ['./resume.component.css']
+})
+export class ResumePagina {
+
+}
