06b4bbe1cd01b77dc38a68f7552ce10f7009634c
Robbe van Hoppe <robbe.vanhoppe@student.pxl.be>
2022-10-27 16:12:37 +0200

    contact pagina zonder opmaak

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators, NgForm } from '@angular/forms';
+import { ContactService } from '../services.service';
+
+@Component({
+  selector: 'portfolio-ContactPagina',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactForm implements OnInit {
+  kleurToggle = true;
+  FormData: FormGroup;
+  constructor(private builder: FormBuilder, private contact: ContactService) { }
+  ngOnInit() {
+    this.FormData = this.builder.group({
+      Fullname: new FormControl('', [Validators.required]),
+      Email: new FormControl('', [Validators.email]),
+      Comment: new FormControl('', [Validators.required])
+    })
+  }
+
+  onSubmit(FormData: any) {
+    console.log(FormData)
+    this.contact.PostMessage(FormData)
+    .subscribe(response => {
+    location.href = 'https://mailthis.to/confirm'
+    console.log(response)
+    }, error => {
+    console.warn(error.responseText)
+    console.log({ error })
+    })
+    }
+
+  veranderKleur() {
+    this.kleurToggle = !this.kleurToggle;
+  }
+
+
+}
