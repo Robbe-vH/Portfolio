8d0cebed702d3a1c5d0e04ab1d441ec209b6048e
RobbevH <robbe.vanhoppe@student.pxl.be>
2023-01-08 17:09:39 +0100

    kaartjes mobile toegevoegd

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,6 +1,6 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope, faGlobeEurope, faLink, faPhoneFlip } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope, faGlobeEurope, faPhoneFlip } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
     selector: 'portfolio-ResumePagina',
@@ -12,5 +12,30 @@ export class ResumePagina {
     envelope = faEnvelope;
     phone = faPhoneFlip;
     globe = faGlobeEurope;
-    linkedIn= faLinkedinIn;
+    linkedIn = faLinkedinIn;
+
+    kaartjes: boolean[] = [true, false, false, false, false]; 
+        
+
+    // Deze functie maakt een slideshow voor de kaartjes met een time-out van 1,5s.
+    ngOnInit() {
+        const sleep = (time: number) => {
+            return new Promise(resolve => setTimeout(resolve, time))
+        }
+        const SlideShow = async () => {
+            while (true) {
+                
+                for (let i = 0; i < 5; i++) {
+                    console.log("Yeet" + i);
+                    this.kaartjes[i] = true;
+                    if (i >= 1) {
+                        this.kaartjes[i - 1] = false;
+                    }
+                    await sleep(15000);
+                }
+                this.kaartjes[4] = false;
+            }
+        }
+        SlideShow();
+    }
 }
