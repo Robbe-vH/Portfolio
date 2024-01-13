4278b65d046699bdde83622aa6ab4a9ba7543442
Robbe van Hoppe <rvanhoppe@qcify.com>
2024-01-13 15:18:54 +0100

    Add logboek images and pdf download

--- src/app/WPL3/wpl3/logboek/logboek.component.ts
+++ src/app/WPL3/wpl3/logboek/logboek.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'logboek',
   templateUrl: './logboek.component.html',
   styleUrls: ['./logboek.component.css']
 })
-export class LogboekComponent implements OnInit {
+export class LogboekComponent {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
 
-  ngOnInit(): void {
+  ) {
+  }
+
+  downloadPdf() {
+    let url: string = 'assets/wpl3/gesprekken/WPL3-Logboek_van_Hoppe_Robbe.pdf'
+    this.http.get(url, {responseType: 'blob'})
+      .subscribe((data: Blob) => {
+        const downloadUrl = URL.createObjectURL(data);
+        const link = document.createElement('a');
+        link.href = downloadUrl;
+        link.download = 'WPL3-Logboek_van_Hoppe_Robbe.pdf'
+        link.click();
+      });
   }
 }
