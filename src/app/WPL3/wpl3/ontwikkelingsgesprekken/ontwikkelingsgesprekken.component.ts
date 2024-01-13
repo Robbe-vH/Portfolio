3e80b501c27e2a6cf1edaa2837ffe11d8c8dd808
Robbe van Hoppe <rvanhoppe@qcify.com>
2024-01-13 15:31:39 +0100

    Ontwikkelingsgesprekken done

--- src/app/WPL3/wpl3/ontwikkelingsgesprekken/ontwikkelingsgesprekken.component.ts
+++ src/app/WPL3/wpl3/ontwikkelingsgesprekken/ontwikkelingsgesprekken.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'ontwikkelingsgesprekken',
   templateUrl: './ontwikkelingsgesprekken.component.html',
   styleUrls: ['./ontwikkelingsgesprekken.component.css']
 })
-export class OntwikkelingsgesprekkenComponent implements OnInit {
+export class OntwikkelingsgesprekkenComponent {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) {
+  }
 
-  ngOnInit(): void {
+  downloadPdf() {
+    let url: string = 'assets/wpl3/gesprekken/WPL3-Feedback_Ontwikkeling_van_Hoppe_Robbe.pdf'
+    this.http.get(url, {responseType: 'blob'})
+      .subscribe((data: Blob) => {
+        const downloadUrl = URL.createObjectURL(data);
+        const link = document.createElement('a');
+        link.href = downloadUrl;
+        link.download = 'WPL3-Feedback_Ontwikkeling_van_Hoppe_Robbe.pdf'
+        link.click();
+      });
   }
 
 }
