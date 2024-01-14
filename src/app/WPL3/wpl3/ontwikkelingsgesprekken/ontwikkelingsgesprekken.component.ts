02fd21712d9b7da54d22f27b390eb753d11f99a4
Robbe van Hoppe <rvanhoppe@qcify.com>
2024-01-14 10:11:26 +0100

    Add all functioneringsgesprekken to html

--- src/app/WPL3/wpl3/ontwikkelingsgesprekken/ontwikkelingsgesprekken.component.ts
+++ src/app/WPL3/wpl3/ontwikkelingsgesprekken/ontwikkelingsgesprekken.component.ts
@@ -13,14 +13,23 @@ export class OntwikkelingsgesprekkenComponent {
   ) {
   }
 
-  downloadPdf() {
-    let url: string = 'assets/wpl3/gesprekken/WPL3-Feedback_Ontwikkeling_van_Hoppe_Robbe.pdf'
+  downloadPdf(file: string) {
+    let url: string = 'assets/wpl3/gesprekken/'
+    let filename = '';
+    if (file == 'ontwikkeling') {
+      filename = 'WPL3-Feedback_Ontwikkeling_van_Hoppe_Robbe.pdf'
+    } else if (file == 'functionering') {
+      filename = 'WPL3-Verslag_Functioneringsgesprek_van_Hoppe_Robbe.pdf'
+
+    }
+      url += filename;
+
     this.http.get(url, {responseType: 'blob'})
       .subscribe((data: Blob) => {
         const downloadUrl = URL.createObjectURL(data);
         const link = document.createElement('a');
         link.href = downloadUrl;
-        link.download = 'WPL3-Feedback_Ontwikkeling_van_Hoppe_Robbe.pdf'
+        link.download = filename;
         link.click();
       });
   }
