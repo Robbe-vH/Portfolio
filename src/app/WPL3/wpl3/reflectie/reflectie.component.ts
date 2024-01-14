0ed3cc7650d9deb12b646ce8227b5143b3903e2c
Robbe van Hoppe <rvanhoppe@qcify.com>
2024-01-14 13:03:02 +0100

    Add reflectie

--- src/app/WPL3/wpl3/reflectie/reflectie.component.ts
+++ src/app/WPL3/wpl3/reflectie/reflectie.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'reflectie',
   templateUrl: './reflectie.component.html',
   styleUrls: ['./reflectie.component.css']
 })
-export class ReflectieComponent implements OnInit {
+export class ReflectieComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private http: HttpClient
+  ) {
   }
 
+  downloadPdf() {
+    let url: string = 'assets/wpl3/reflectie/Reflectie_leerdoelen_van_Hoppe_Robbe.pdf';
+
+    this.http.get(url, {responseType: 'blob'})
+      .subscribe((data: Blob) => {
+        const downloadUrl = URL.createObjectURL(data);
+        const link = document.createElement('a');
+        link.href = downloadUrl;
+        link.download = 'Reflectie_leerdoelen_van_Hoppe_Robbe.pdf';
+        link.click();
+      });
+  }
 }
